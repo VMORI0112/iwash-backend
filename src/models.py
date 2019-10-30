51b80b46c5dc4be1630912e2b2184caaa4c37d66
samir benzada <samirbenzada@gmail.com>
2019-10-30 09:59:56 -0400

    update mysql

--- src/models.py
+++ src/models.py
@@ -5,13 +5,16 @@ db = SQLAlchemy()
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    firstname = db.Column(db.String(120), nullable=False)
+    lastname = db.Column(db.String(120) )
     password = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
-        return '<Person %r>' % self.username
+        return '<Users %r>' % self.email
 
     def serialize(self):
         return {
-            "username": self.username,
-            "email": self.email
+            "email": self.email,
+            "firstname": self.firstname,
+            "lastname": self.lastname
         }
